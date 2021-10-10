ba864cec0ca0630aad2804a5cb2fb06912f43813
SAKSHI993 <76905956+SAKSHI993@users.noreply.github.com>
2021-10-11 01:06:58 +0530

    Add files via upload

---
+++ week2/q7.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+    public:
+    void sort012(int a[], int n)
+    {
+    int low=0, mid = 0,high = n-1;
+    while(mid<=high){
+        switch(a[mid]){
+            case 0:
+                swap(a[low], a[mid]);
+                low++;
+                mid++;
+                break;
+            case 1:
+                mid++;
+                break;
+            case 2:
+                swap(a[high], a[mid]);
+                high--;
+                break;
+        }
+        
+    }
+    }
+};
